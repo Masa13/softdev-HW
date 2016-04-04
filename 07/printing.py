5dbdb2d2d86e9a70e4c8914019a4b4149e943410
Masa13 <masahero55@gmail.com>
2016-04-04 10:08:11 -0400

    classwork .-.

--- 07/printing.py
+++ 07/printing.py
@@ -1,2 +1,13 @@
 def repeat(string):
     return lambda x: string * x
+
+def repeat(word):
+    def times(x):
+        return word * x
+    return times
+
+r1 = repeat('hello')
+r2 = repeat('cool')
+print r1(2)
+print r2(2)
+print repeat('cool')(3)
