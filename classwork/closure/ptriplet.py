d1da0461e829a84c6deeb275f07721b9c9c74227
Masa13 <masahero55@gmail.com>
2016-04-11 09:38:58 -0400

    heh

--- classwork/closure/ptriplet.py
+++ classwork/closure/ptriplet.py
@@ -1,8 +1,6 @@
-######################################################
-######################################################
-#Pythagorean Triplets 
+##############Pythagorean Triplets##############
 
-def pt(n):
+def pta(n):
     triplets=[]
     for c in xrange(1,n):
         for a in xrange(1,c):
@@ -11,10 +9,13 @@ def pt(n):
                     triplets.append([b,a,c])
     return triplets
 
-print pt(100)
+print 'Testing pt1...'
+print pta(20)
 
+
+#Classcode
 '''
-def pt1(n):
+def ptb(n):
     triplets=[]
     for a in range(1,n):
         for b in range(1,n):
@@ -23,10 +24,41 @@ def pt1(n):
                     triplets.append([a,b,c])
     return triplets
 
-print pt1(100)
+print ptb(100)
 '''
 
-##################################################
 
-def pt2(n):
-    return([a,b,c] 
+def pt2a(n):
+    return[(a,b,c)
+           for c in xrange(n)
+           for b in xrange(c)
+           for a in xrange(b)
+           if a**2 + b**2 == c**2]
+print '\n\nTesting pt2...'
+print pt2a(20)
+
+#Classcode
+'''
+def pt2b(n):
+    return [(a,b,c)
+            for a in range(1,n)
+            for b in range(1,n)
+            for c in range(1,n)
+            if a*a + b*b == c*c]
+'''
+
+#####################Quicksort######################
+'''
+(1)Pick a PIVOT
+(2)Partition into 2 Lists, s.t.
+   *such that all v's < p are in lower half
+   *such that all v's > p are in upper half
+   *Pivot in F.R.P
+(3) qsort(LH) + p + (qsortUH)
+'''
+
+def qsort(list):
+    lower=[]
+    upper=[]
+
+    if len()
